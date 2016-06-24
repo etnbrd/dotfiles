1d6c93620b82cf407a5d530f7f41af1cd2917354
etnbrd <etn@etnbrd.com>
2016-06-24 16:51:51 +0200

    stuffs

---
+++ google-cloud[dev]/path.zsh
@@ -0,0 +1,5 @@
+# # The next line updates PATH for the Google Cloud SDK.
+source '/home/etn/Downloads/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables shell command completion for gcloud.
+source '/home/etn/Downloads/google-cloud-sdk/completion.zsh.inc'
