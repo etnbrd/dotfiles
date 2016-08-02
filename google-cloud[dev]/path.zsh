c92d1424c5fc8eb75a3b5ef7fb11fba0ab8e59da
etnbrd <etn@etnbrd.com>
2016-08-02 17:05:24 +0200

    reorganisation for headless dev env

--- google-cloud[dev]/path.zsh
+++ google-cloud[dev]/path.zsh
@@ -1,5 +1,5 @@
 # # The next line updates PATH for the Google Cloud SDK.
-source '/home/etn/Downloads/google-cloud-sdk/path.zsh.inc'
+source '/opt/google-cloud-sdk/path.zsh.inc'
 
 # The next line enables shell command completion for gcloud.
-source '/home/etn/Downloads/google-cloud-sdk/completion.zsh.inc'
+source '/opt/google-cloud-sdk/completion.zsh.inc'
